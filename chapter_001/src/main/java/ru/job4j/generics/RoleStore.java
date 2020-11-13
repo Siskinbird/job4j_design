1ccd5413e0ac5c77e42ff85390e3701c3eb842f6
Siskinbird <dimachig@gmail.com>
2020-11-13 10:28:29 +0300

    5.2.2. Реализовать Store<T extends Base> [#385873]

---
+++ chapter_001/src/main/java/ru/job4j/generics/RoleStore.java
@@ -0,0 +1,34 @@
+package ru.job4j.generics;
+
+
+public class RoleStore<Role extends Base> implements Store<Role> {
+
+    private final Store<Role> store = new MemStore<>();
+
+    @Override
+    public void add(Role model) {
+        store.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, Role model) {
+        if (model.getId().equals(id)){
+            delete(id);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        if (store.findById(id) == null) {
+            return false;
+        }else  {
+            return true;
+        }
+    }
+
+    @Override
+    public Role findById(String id) {
+        return null;
+    }
+}
