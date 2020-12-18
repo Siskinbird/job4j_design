1d935ee324492df29411c4d034776dfcd44d91db
Siskinbird <dimachig@gmail.com>
2020-12-18 14:35:12 +0300

    1. Создать модель User [#385879]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -7,22 +7,4 @@ public class User {
     private String name;
     private int age;
     private Calendar birthday;
-
-    public void newUser(String name, int age, Calendar birthday) {
-        this.name = name;
-        this.age = age;
-        this.birthday = birthday;
-    }
-
-    public void setBirthday(Calendar birthday) {
-        this.birthday = birthday;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
