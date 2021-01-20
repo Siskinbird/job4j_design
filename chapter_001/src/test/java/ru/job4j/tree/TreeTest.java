40af7032b209d59d01f3b1fb4ae809b84f180e7e
Siskinbird <dimachig@gmail.com>
2021-01-20 10:48:49 +0300

    2. Добавить метод boolean isBinary() [#385869]

--- chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
+++ chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tree;
 
 import org.junit.Test;
+
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -104,6 +106,60 @@ public class TreeTest {
                 tree.findBy(7).isPresent(),
                 is(false)
         );
+    }
+
+    @Test
+    public void isBinary() {
+        Tree<Integer> tree = new Tree<>(16);
+        tree.add(10, 9);
+        tree.add(10, 11);
+        tree.add(42, 39);
+        tree.add(42, 48);
+        assertThat(
+                tree.isBinary(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void noBinary() {
+        Tree<Integer> tree = new Tree<>(16);
+        tree.add(16, 9);
+        tree.add(16, 10);
+        tree.add(16, 11);
+        tree.add(16, 48);
+        tree.add(16, 44);
+        tree.add(16, 10);
+        tree.add(16, 19);
+        tree.add(10, 49);
+        assertThat(
+                tree.isBinary(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void findBy() {
+        Tree<Integer> tree = new Tree<>(42);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(42).isPresent(),
+                is(true));
+    }
+
+    @Test
+    public void noFoundBy() {
+        Tree<Integer> tree = new Tree<>(66);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(42).isPresent(),
+                is(false));
     }
 
 }
