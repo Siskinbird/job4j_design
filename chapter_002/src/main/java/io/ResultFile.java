501d77d573f97a8d780ad82a2e52e707efc433bb
Siskinbird <dimachig@gmail.com>
2021-02-15 17:02:48 +0300

    chapterFixes

--- chapter_002/src/main/java/io/ResultFile.java
+++ chapter_002/src/main/java/io/ResultFile.java
@@ -5,8 +5,8 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        String result = Double.toString(calculator.add(11, 11));
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        String result = Double.toString(calculator.add(1, 11));
+        try (FileOutputStream out = new FileOutputStream("C:\\Projects\\job4j_design\\chapter_002\\src\\main\\java\\io\\files\\result.txt")) {
             out.write(result.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
