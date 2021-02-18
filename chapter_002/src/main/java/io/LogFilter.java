2da42b6a0d59cf92fcd242a20d5c5dd770b2be93
Siskinbird <dimachig@gmail.com>
2021-02-18 09:39:05 +0300

    0.3. BufferedReader. [#385900]

--- chapter_002/src/main/java/io/LogFilter.java
+++ chapter_002/src/main/java/io/LogFilter.java
@@ -2,28 +2,25 @@ package io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class LogFilter {
-
-    public static List<String> filter(String file) {
+    public static List<String> filter(String file) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-           List<String> lines = in.lines().filter(i -> i.contains("404")).collect(Collectors.toList());
-            String[] lines1 = lines.toString().split(System.lineSeparator());
-            for (String s : lines) {
-                if (!s.endsWith("404")) {
-                    System.out.println(s);
+            List<String> result = new ArrayList<>();
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] arr = line.split(" ");
+                if (arr[arr.length - 2].equals("404")) {
+                    result.add(line);
                 }
-
-           }
-        } catch (Exception e) {
-            e.printStackTrace();
+            }
+            return result;
         }
-        return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> log = filter("./io-data/log.txt");
         System.out.println(log);
 
