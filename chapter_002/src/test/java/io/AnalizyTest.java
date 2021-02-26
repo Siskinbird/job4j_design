e9059af45edf449bb7033ced519c75ca2f42c970
Siskinbird <dimachig@gmail.com>
2021-02-26 16:22:29 +0300

    3.0. Тестирование IO [#385897]

--- chapter_002/src/test/java/io/AnalizyTest.java
+++ chapter_002/src/test/java/io/AnalizyTest.java
@@ -3,42 +3,28 @@ package io;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-
-
-
 import java.io.*;
 
 public class AnalizyTest {
+    String separator = System.lineSeparator();
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void unavailable() throws IOException {
-        File source = folder.newFile("source.txt");
-        File target = folder.newFile("target.txt");
-        try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01"
-                    + "\n200 10:57:01"
-                    + "\n400 10:58:01"
-                    + "\n200 10:59:01"
-                    + "\n500 11:01:02"
-                    + "\n200 11:02:02");
+    public void whenReadFile() throws IOException {
+        StringBuilder buildString = new StringBuilder();
+        Analizy analizy = new Analizy();
+        File target = folder.newFile("target");
+        analizy.unavailable("./data/server.txt", target.getAbsolutePath());
+        try (BufferedReader read = new BufferedReader(new FileReader(target.getAbsolutePath()))) {
+            for (String line = read.readLine(); line != null; line = read.readLine()) {
+                buildString.append(line).append(separator);
+            }
         }
-        Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
-        StringBuilder rsl = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            in.lines().forEach(rsl::append);
-        }
-        assertThat(rsl.toString(), is("10:58:01 server down"
-                + "10:59:01 server up"
-                + "11:01:02 server down"
-                + "11:02:02 server up"));
+        assertThat(buildString.toString(), is("10:57:01 - 10:59:01" + separator + "11:01:02 - 11:02:02" + separator));
     }
 }
