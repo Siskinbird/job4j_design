bb0084cc5b94f0da01fea878a37abc00c18bca22
Siskinbird <dimachig@gmail.com>
2021-03-12 12:51:18 +0300

    5.2. Архивировать проект [#385907]

--- chapter_002/src/main/java/zipper/ArgZip.java
+++ chapter_002/src/main/java/zipper/ArgZip.java
@@ -3,15 +3,24 @@ package zipper;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class ArgZip - Argument validation
+ * @author Dmitry Chizhov (dimachig@gmail.com)
+ * @version 1.00
+ * @since 12.03.21
+ */
+
 public class ArgZip {
 
     private final String[] args;
-    private final HashMap<String, String> values = new HashMap<>();
+    private  HashMap<String, String> values = new HashMap<>();
 
     public ArgZip(String[] args) {
         this.args = args;
     }
-
+/*
+Map for args
+ */
     public Map<String, String> argsValues() {
         for (String argument : args) {
             String[] arr = argument.split("=");
@@ -22,22 +31,30 @@ public class ArgZip {
         }
         return values;
     }
-
+/*
+argsValues validation
+ */
     public boolean valid() {
-        if (values.size() != 3) {
+        if (this.argsValues().size() != 3) {
             throw new IllegalArgumentException();
         }
        return true;
     }
-
+/*
+getDirectory
+ */
     public String directory() {
         return values.get("d");
     }
-
+/*
+getExclude
+ */
     public String exclude() {
         return values.get("e");
     }
-
+/*
+getOutput file
+ */
     public String output() {
         return values.get("o");
     }
--- chapter_002/src/main/java/zipper/Zip.java
+++ chapter_002/src/main/java/zipper/Zip.java
@@ -1,29 +1,54 @@
 package zipper;
 
+import io.SearchFiles;
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * Class Zip - Implements packing files into an archive
+ * @author Dmitry Chizhov (dimachig@gmail.com)
+ * @version 1.00
+ * @since 12.03.21
+ */
+
 public class Zip {
+    ArgZip argZip;
 
-    public void packFiles(List<Path> sources, File target) throws FileNotFoundException {
-        try (ZipOutputStream zipPack = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (Path source : sources) {
-                zipPack.putNextEntry(
-                        new ZipEntry(source.toFile().getAbsolutePath()));
-                try (BufferedInputStream out = new BufferedInputStream(
-                        new FileInputStream(source.toFile().getPath()))) {
-                    zipPack.write(out.readAllBytes());
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public Zip(ArgZip argZip) {
+        this.argZip = argZip;
+
+        if (!argZip.valid()) {
+            throw new IllegalArgumentException("Not all arguments");
         }
-
     }
+
+    /**
+     * Method packFiles() - Implements packing files into an archive
+     * @param sources list of paths
+     * @param target file to write
+     * @throws IOException - exception
+     */
+
+    public void packFiles(List<Path> sources, File target) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(argZip.exclude()));
+        Files.walkFileTree(Paths.get(argZip.directory()), searcher);
+        sources.forEach(searcher.getPaths()::add);
+        for (Path source : sources) {
+            packSingleFile(source.toFile(), target);
+        }
+    }
+
+    /**
+     * Method packSingleFile() - Implements packing a file into an archive
+     * @param source - The file to be packed
+     * @param target - Zipped file
+     */
 
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(
@@ -39,9 +64,11 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
+        ArgZip argZip = new ArgZip(args);
+        Zip zip = new Zip(argZip);
 
-        new Zip().packSingleFile(
+        new Zip(argZip).packSingleFile(
                 new File("./chapter_005/pom.xml"),
                 new File("./chapter_005/pom.zip")
         );
@@ -51,7 +78,6 @@ public class Zip {
         list.add(path1);
         list.add(path2);
 
-        new Zip().packFiles(list,
-                new File("./chapter_002/zipped.zip"));
+        zip.packFiles(list, new File(argZip.output()));
     }
 }
