0b5ff9fd15fec0dd99df8f280f85ae42861f149b
Siskinbird <dimachig@gmail.com>
2021-03-28 12:53:10 +0300

    1. Бот [#385922]

--- chapter_002/src/main/java/io/EchoServer.java
+++ chapter_002/src/main/java/io/EchoServer.java
@@ -9,25 +9,40 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        int count = 0;
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
+                System.out.println("Client request # " + (++count));
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
+                    System.out.println("Client:" + str);
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     if (str != null && !str.isEmpty()) {
-                        if (str.contains("Bye")) {
-                            out.write("Bye bye\r\r\r\n".getBytes());
+                        if (str.contains("Hello")) {
+                            out.write(("Hey! I am a test server, a young programmer wrote me.\n"
+                                    + "To find out which commands\n"
+                                    + "I react to write \"Help\"\r\r\r\n").getBytes());
+                        } else if (str.contains("Help")) {
+                            out.write(("Request command: \n"
+                                    + "Hello - Hello\n"
+                                    + "Exit - Exit\n"
+                                    + "Help - Help\n"
+                                    + "These are all commands."
+                                    + "An attempt to enter a third-party command can damage your computer.\r\r\r\n").getBytes());
+                        } else if (str.contains("Exit")) {
+                            out.write("Connection lost\r\r\r\n".getBytes());
                             out.flush();
                             socket.close();
                             break;
-                        }
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        } else {
+                            out.write("Wrong request. Your computer is damaged. Try - Help\r\n\r\n".getBytes());
                         }
                     }
                 }
             }
         }
     }
+}
