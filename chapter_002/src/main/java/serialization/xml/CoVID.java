33c25b7ddc41e37bc7500df7a97ab80d16c36e38
Siskinbird <dimachig@gmail.com>
2021-04-01 12:53:40 +0300

    5. Преобразование JSON в POJO. JsonObject [#385915]

--- chapter_002/src/main/java/serialization/xml/CoVID.java
+++ chapter_002/src/main/java/serialization/xml/CoVID.java
@@ -1,36 +1,35 @@
 package serialization.xml;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import javax.xml.bind.annotation.*;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @XmlRootElement(name = "virusBase")
 @XmlAccessorType(XmlAccessType.FIELD)
 
 public class CoVID {
-
-    @XmlAttribute
-    private boolean contagiousness;
-
-    @XmlAttribute
-    private int since;
-    private CoronaVir coronaVir;
-
-    @XmlElementWrapper(name = "properties")
-    @XmlElement(name = "property")
-    private String[] properties;
-
-    public CoVID() { }
+    private final boolean contagiousness;
+    private final int since;
+    private final CoronaVir coronaVir;
+    private final String[] properties;
 
     public CoVID(boolean contagiousness, int since, CoronaVir coronaVir, String... properties) {
+
         this.contagiousness = contagiousness;
         this.since = since;
         this.coronaVir = coronaVir;
         this.properties = properties;
+    }
+
+    public boolean isContagiousness() {
+        return contagiousness;
+    }
+
+    public int getSince() {
+        return since;
     }
 
     @Override
@@ -43,29 +42,30 @@ public class CoVID {
                 + '}';
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        /* JSONObject из json-строки строки */
+        JSONObject jsonStrain = new JSONObject("{\"strain\":\"COVID-19\"}");
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("Very hi");
+        list.add(" Deadly dangerous");
+        list.add(" You have no chance");
+        JSONArray jsonProperties = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
         final CoVID coVID = new CoVID(true, 2020, new CoronaVir("COVID-19"), "Very hi, Deadly dangerous, You have no chance");
-        /* Получаем контекст для доступа к АПИ */
-        JAXBContext context = JAXBContext.newInstance(CoVID.class);
-        /* Создаем сериализатор */
-        Marshaller marshaller = context.createMarshaller();
-        /* Указываем, что нам нужно форматирование */
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
-        try (StringWriter writer = new StringWriter()) {
-            //Сериализуем
-            marshaller.marshal(coVID, writer);
-            xml = writer.getBuffer().toString();
-            System.out.println(xml);
-        }
-        /* Для десериализации нам нужно создать десериализатор */
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            // Дисериализуем
-            CoVID result = (CoVID) unmarshaller.unmarshal(reader);
-            System.out.println(result);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("contagiousness", coVID.isContagiousness());
+        jsonObject.put("since", coVID.getSince());
+        jsonObject.put("coronaVir", jsonStrain);
+        jsonObject.put("properties", jsonProperties);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект coVID в json-строку */
+        System.out.println(new JSONObject(coVID).toString());
+
         }
     }
-}
-
 
