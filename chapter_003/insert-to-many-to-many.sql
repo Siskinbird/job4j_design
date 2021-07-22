7b585ebb59d18318de48b037e5c59752db6ec5c2
Dmitrii Chizhov <59995752+Siskinbird@users.noreply.github.com>
2021-07-22 16:44:28 +0300

    Add files via upload
    Связи между таблицами [#373615]

---
+++ chapter_003/insert-to-many-to-many.sql
@@ -0,0 +1,23 @@
+insert into person_names(name) values ('Tyotya Asya');
+insert into person_names(name) values ('Mister Propper');
+insert into person_names(name) values ('Doctor Pepper');
+
+insert into phobies(name) values ('Nosophobia');
+insert into phobies(name) values ('Algophobia');
+insert into phobies(name) values ('Glossophobia');
+insert into phobies(name) values ('Nosophobia');
+insert into phobies(name) values ('Eisoptrophobia');
+insert into phobies(name) values ('Ophidiophobia');
+
+insert into persons_phobies(person_name_id, phobia_id) values (1, 1);
+insert into persons_phobies(person_name_id, phobia_id) values (1, 2);
+insert into persons_phobies(person_name_id, phobia_id) values (1, 3);
+insert into persons_phobies(person_name_id, phobia_id) values (1, 4);
+insert into persons_phobies(person_name_id, phobia_id) values (1, 5);
+insert into persons_phobies(person_name_id, phobia_id) values (2, 2);
+insert into persons_phobies(person_name_id, phobia_id) values (2, 4);
+insert into persons_phobies(person_name_id, phobia_id) values (2, 5);
+insert into persons_phobies(person_name_id, phobia_id) values (3, 1);
+insert into persons_phobies(person_name_id, phobia_id) values (3, 3);
+insert into persons_phobies(person_name_id, phobia_id) values (3, 5);
+insert into persons_phobies(person_name_id, phobia_id) values (3, 6);
