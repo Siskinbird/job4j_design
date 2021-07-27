7b79dbc187f6e13917061696975e5ed7f943a8f3
Dmitrii Chizhov <59995752+Siskinbird@users.noreply.github.com>
2021-07-27 17:32:59 +0300

    Фильтры [#1732]
    Ух блэт

--- chapter_003/insert.sql
+++ chapter_003/insert.sql
@@ -1,38 +1,77 @@
---Devices
-insert into devices(name, price)
-values('Samsung', 3.33);
-insert into devices(name, price)
-values('Philips', 4.44);
-insert into devices(name, price)
-values('Iphone', 5.55);
-insert into devices(name, price)
-values('Nokia', 6.66);
-
---People
-insert into people(name)
-values('Vasa');
-insert into people(name)
-values('Peta');
-insert into people(name)
-values('Nina');
-insert into people(name)
-values('Viktor');
-
---Devices_people
-insert into devices_people(device_id, people_id)
-values(5, 1);
-insert into devices_people(device_id, people_id)
-values(6, 1);
-insert into devices_people(device_id, people_id)
-values(7, 1);
-insert into devices_people(device_id, people_id)
-values(5, 2);
-insert into devices_people(device_id, people_id)
-values(6, 2);
-insert into devices_people(device_id, people_id)
-values(6, 3);
-insert into devices_people(device_id, people_id)
-values(7, 4);
-insert into devices_people(device_id, people_id)
-values(8, 4);
+insert into type(name)
+values('Молоко');
+insert into type(name)
+values('Сыр');
+insert into type(name)
+values('Мороженное');
+insert into product(name, type_id, expired_date, price)
+values('Слеза_коровы', 1, 'January 30, 2022', 3.50);
+insert into product(name, type_id, expired_date, price)
+values('Кофейная_разбавка', 1, 'June 30, 2022', 11.50);
+insert into product(name, type_id, expired_date, price)
+values('Любиой_женщины', 1, 'January 30, 2012', 43.50);
+insert into product(name, type_id, expired_date, price)
+values('Детская_радость', 1, 'August 14, 2022', 33.50);
+insert into product(name, type_id, expired_date, price)
+values('Ушедшая_Надежда', 1, 'April 22, 2022', 17.50);
+insert into product(name, type_id, expired_date, price)
+values('Отпуск_пастуха', 1, 'February 11, 2023', 3.50);
+insert into product(name, type_id, expired_date, price)
+values('Вода_в_диплом', 1, 'April 30, 2041', 99.99);
+insert into product(name, type_id, expired_date, price)
+values('Козье', 1, 'May 30, 2017', 3.50);
+insert into product(name, type_id, expired_date, price)
+values('Бычачье', 1, 'August 11, 2022', 53.50);
+insert into product(name, type_id, expired_date, price)
+values('Божей_коровки', 1, 'September 3, 2032', 93.50);
+insert into product(name, type_id, expired_date, price)
+values('Последний_день', 1, 'January 15, 1997', 0.50);
+insert into product(name, type_id, expired_date, price)
+values('13_Негритят', 1, 'October 12, 2024', 13.50);
+insert into product(name, type_id, expired_date, price)
+values('Святой_коровы', 1, 'November 20, 2021', 153.50);
+insert into product(name, type_id, expired_date, price)
+values('Слеза_коровы', 1, 'January 30, 2022', 3.50);
+insert into product(name, type_id, expired_date, price)
+values('Куриное_сердце', 2, 'June 23, 2022', 11.30);
+insert into product(name, type_id, expired_date, price)
+values('Тизер',  2, 'January 11, 2012', 35.50);
+insert into product(name, type_id, expired_date, price)
+values('Пешеходский',  2, 'August 14, 2022', 11.75);
+insert into product(name, type_id, expired_date, price)
+values('Мадам',  2, 'April 23, 2022', 65.60);
+insert into product(name, type_id, expired_date, price)
+values('Партизан',  2, 'February 15, 2023', 43.50);
+insert into product(name, type_id, expired_date, price)
+values('Ламберт',  2, 'April 13, 2041', 64.99);
+insert into product(name, type_id, expired_date, price)
+values('Иуда', 2, 'May 22, 2017', 57.30);
+insert into product(name, type_id, expired_date, price)
+values('ЗаЦарелло', 2, 'August 11, 2022', 23.50);
+insert into product(name, type_id, expired_date, price)
+values('Тяжелый_подсллнечный', 2, 'September 3, 2032', 70.50);
+insert into product(name, type_id, expired_date, price)
+values('Майонезный', 2, 'January 7, 1997', 10.50);
+insert into product(name, type_id, expired_date, price)
+values('Адам', 2, 'October 1, 2024', 173.50);
+insert into product(name, type_id, expired_date, price)
+values('Финиш', 2, 'November 25, 2021', 153.50);
+insert into product(name, type_id, expired_date, price)
+values('Пироженое', 3, 'June 13, 2022', 11.30);
+insert into product(name, type_id, expired_date, price)
+values('Отмороженое',  3, 'January 21, 2012', 35.50);
+insert into product(name, type_id, expired_date, price)
+values('Замороженое',  3, 'August 4, 2022', 11.75);
+insert into product(name, type_id, expired_date, price)
+values('Недомороженое',  3, 'April 21, 2022', 65.60);
+insert into product(name, type_id, expired_date, price)
+values('Обмороженое',  3, 'February 17, 2023', 43.50);
+insert into product(name, type_id, expired_date, price)
+values('Размороженое',  3, 'April 11, 2041', 64.99);
+insert into product(name, type_id, expired_date, price)
+values('Перемороженое', 3, 'May 24, 2017', 57.30);
+insert into product(name, type_id, expired_date, price)
+values('Примороженое', 3, 'August 17, 2022', 23.50);
+insert into product(name, type_id, expired_date, price)
+values('Подмороженое', 3, 'September 3, 2032', 70.50);
 
--- chapter_003/requests.sql
+++ chapter_003/requests.sql
@@ -1,14 +1,65 @@
--- Avg device prise
-select avg(price) as Средняя_цена_устройств from devices;
+-- Написать запрос получение всех продуктов с типом "СЫР"
 
--- Avg price for each person
-select p.name as Имя, avg(dev.price) as Средняя_цена 
-from people as p 
-join devices_people dp
-on p.id = dp.people_id
-join devices as dev
-on dp.device_id = dev.id
-group by p.name
+select p.name as Имя_продукта, tn.name as Тип_продукта
+from product as p 
+join type tn
+on tn.id = p.type_id
+where tn.name like '%Сыр%';
 
--- Avg price for each person is more than five
-having avg(dev.price) > 5;
+-- Написать запрос получения всех продуктов, у кого в имени есть слово "мороженое"
+
+select p.name as Имя_продукта, tn.name as Тип_продукта 
+from product as p 
+join type tn
+on tn.id = p.type_id
+where tn.name like '%Мороженное%';
+
+--Написать запрос, который выводит все продукты, срок годности которых уже истек
+
+select name as Имя_продукта, expired_date as Просрочены
+from product
+where (expired_date < current_date);
+
+--Написать запрос, который выводит самый дорогой продукт.
+
+select name, price
+from product
+where price = (select max(price) from product);
+
+--Написать запрос, который выводит для каждого типа количество продуктов к нему принадлежащих. В виде имя_типа, количество
+
+select tn.name, count(tn.name) 
+FROM product
+join type tn
+on tn.id = product.type_id
+group by tn.name;
+
+--Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+
+select p.name as Имя_продукта, tn.name as Тип_продукта
+from product as p 
+join type tn
+on tn.id = p.type_id
+where tn.name like '%Сыр%' OR tn.name like '%Молоко%';
+
+--Написать запрос, который выводит тип 
+--продуктов, которых осталось меньше 10 штук. 
+--Под количеством подразумевается количество 
+--продуктов определенного типа. 
+--Например, если есть тип "СЫР" и 
+--продукты "Сыр плавленный" и "Сыр моцарелла", которые ему принадлежат, 
+--то количество продуктов типа "СЫР" будет 2. 
+
+SELECT tn.name, count(tn.name) 
+FROM product
+join type tn
+on tn.id = product.type_id
+GROUP BY tn.name 
+HAVING count(tn.name) < 10;
+
+--Вывести все продукты и их тип.
+
+select p.name as Имя_продукта, tn.name as Тип_продукта
+from product as p 
+join type tn
+on tn.id = p.type_id;
