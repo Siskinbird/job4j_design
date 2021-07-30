e5285b46a4cc2f9446950ec45de969fb65f1adfc
Dmitrii Chizhov <59995752+Siskinbird@users.noreply.github.com>
2021-07-30 14:07:25 +0300

    0. Join [#6862]

--- chapter_003/devices.sql
+++ chapter_003/devices.sql
@@ -1,16 +1,10 @@
-create table devices(
-    id serial primary key,
-    name varchar(255),
-    price float
-);
-
-create table people(
+create table owners(
     id serial primary key,
     name varchar(255)
 );
 
-create table devices_people(
+create table devices(
     id serial primary key,
-    device_id int references devices(id),
-    people_id int references people(id)
+    name varchar(255),
+    owner_id int references owners(id)
 );
---
+++ chapter_003/teensTable.sql
@@ -0,0 +1,35 @@
+/* Create
+create table gender(
+	id serial primary key,
+	gender varchar(6)
+);
+
+create table teens(
+	id serial primary key,
+	name varchar(12),
+	gender_id int references gender(id)
+);
+*/
+
+/* Insert
+insert into gender(gender) values('male');
+insert into gender(gender) values('female');
+
+insert into teens(name, gender_id)
+values('Masha', 2);
+insert into teens(name, gender_id)
+values('Misha', 1);
+insert into teens(name, gender_id)
+values('Dasha', 2);
+insert into teens(name, gender_id)
+values('Sasha', 1);
+*/
+
+/* Select
+select n1.name as Имена, n2.name as Именя, 
+concat(n1.gender_id, ' ', n2.gender_id) as Пары 
+from teens n1 
+cross join teens as n2
+where concat(n1.gender_id, ' ', n2.gender_id) = '1 2' 
+or concat(n1.gender_id, ' ', n2.gender_id) = '2 1';
+*/
